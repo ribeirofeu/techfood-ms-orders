66a5b3bd24cf05e7e0f87244bf8d3765f4a05492
RicardoAlmeida7 <ricardosalmeida.7@gmail.com>
2024-03-19 23:36:36 -0300

    adding new unit tests

---
+++ src/test/java/com/fiap/techfood/infrastructure/repository/entity/OrderEntityTest.java
@@ -0,0 +1,51 @@
+package com.fiap.techfood.infrastructure.repository.entity;
+
+import com.fiap.techfood.domain.order.OrderStatus;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class OrderEntityTest {
+
+    @Test
+    public void testOrderEntityBuilder() {
+        // Arrange
+        Long orderId = 1L;
+        CustomerEntity customer = new CustomerEntity();
+        BigDecimal totalValue = BigDecimal.valueOf(100.00);
+        OrderStatus status = OrderStatus.RECEIVED;
+        OffsetDateTime createdDateTime = OffsetDateTime.now();
+        OffsetDateTime receivedDateTime = OffsetDateTime.now().plusHours(1);
+        String notes = "Test notes";
+        String qrCode = "Test QR code";
+
+        // Act
+        OrderEntity orderEntity = OrderEntity.builder()
+                .id(orderId)
+                .customer(customer)
+                .totalValue(totalValue)
+                .status(status)
+                .createdDateTime(createdDateTime)
+                .receivedDateTime(receivedDateTime)
+                .notes(notes)
+                .qrCode(qrCode)
+                .items(Collections.emptyList())
+                .build();
+
+        // Assert
+        assertNotNull(orderEntity);
+        assertEquals(orderId, orderEntity.getId());
+        assertEquals(customer, orderEntity.getCustomer());
+        assertEquals(totalValue, orderEntity.getTotalValue());
+        assertEquals(status, orderEntity.getStatus());
+        assertEquals(createdDateTime, orderEntity.getCreatedDateTime());
+        assertEquals(receivedDateTime, orderEntity.getReceivedDateTime());
+        assertEquals(notes, orderEntity.getNotes());
+        assertEquals(qrCode, orderEntity.getQrCode());
+    }
+}
